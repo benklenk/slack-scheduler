731fe942aeba517544949ddf76f639ed82828f69
Nina Thiebaut <thiebautnina@gmail.com>
2018-07-24 09:41:37 -0700

    create new user based on token

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
 
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+var models = require('./models.js');
+
+var User = models.User
+var Task = models.Task
+var Meeting = models.Meeting
+var Request = models.Request
+
+var app = express();
+const server = require('http').Server(app);
+var oAuth2Client;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // If modifying these scopes, delete credentials.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-const TOKEN_PATH = 'token.json';
+const SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/userinfo.profile'];
+
+mongoose.connection.on('connected', function(){
+  console.log("Successfully Connected");
+})
+mongoose.connect(process.env.MONGODB_URI);
+
+
+var token = null;
 
 // Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
@@ -13,31 +40,29 @@ fs.readFile('credentials.json', (err, content) => {
   authorize(JSON.parse(content), listEvents);
 });
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
 function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
 
+  oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[1] + "/google/callback");
+
+  User.findOne({slackId: "d"}, function(err, result){
+    if(err || result===null){
+      console.log("user does not exist");
+    }else{
+       token = result.gAccessToken
+    }
+  })
+
+  console.log(token)
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
+  fs.readFile(JSON.stringify(token), (err, token) => {
     if (err) return getAccessToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
     callback(oAuth2Client);
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: 'offline',
@@ -48,44 +73,50 @@ function getAccessToken(oAuth2Client, callback) {
     input: process.stdin,
     output: process.stdout,
   });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return callback(err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
 }
 
-/**
- * Lists the next 10 events on the user's primary calendar.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
 function listEvents(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
-  calendar.events.list({
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const events = res.data.items;
-    if (events.length) {
-      console.log('Upcoming 10 events:');
-      events.map((event, i) => {
-        const start = event.start.dateTime || event.start.date;
-        console.log(`${start} - ${event.summary}`);
-      });
-    } else {
-      console.log('No upcoming events found.');
-    }
-  });
+  console.log("Successfully Connected to your googleCalendar ")
 }
+
+app.get('/google/callback', (req, res) => {
+  console.log(req.query.code);
+  code = req.query.code;
+
+  oAuth2Client.getToken(code, (err, token1) => {
+    if (err) return function(err){
+      console.log('we got an error over here')
+    };
+    oAuth2Client.setCredentials(token1);
+    // Store the token to disk for later program executions
+    // fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+    //   if (err) console.error(err);
+    //   console.log('Token stored to', TOKEN_PATH);
+    // });
+    token = token1;
+    console.log(token)
+
+    var newUser = new User({
+      gProfileId: 'HELLO',
+      gAccessToken: token.access_token,
+      gRefreshToken: token.expiry_date,
+      slackId: 'helloImASlackUser',
+      slackUsername: 'benny',
+      slackEmail: 'benito@slack.org',
+      slackDMiD: []
+    })
+    newUser.save(function(err,result){
+      if(err){
+        console.log(err)
+      }else{
+        console.log(result);
+      }
+    })
+
+      callback(oAuth2Client);
+  });
+
+  res.send('user created motha fucka');
+})
+
+app.listen(3000);
